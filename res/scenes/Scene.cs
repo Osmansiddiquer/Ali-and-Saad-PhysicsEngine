c0502440fa11166e047da4c7feaf37d9cb144684
Ali-Ahmad0 <aliahmadbhs@gmail.com>
2024-03-08 14:10:45 +0500

    Spawning balls scene

--- res/scenes/Scene.cs
+++ res/scenes/Scene.cs
@@ -10,19 +10,22 @@ namespace PhysicsEngine.res.scenes;
 public class Scene : PhysicsWorld2D
 {
     // Member variables
-
     private static List<PhysicsBody2D> bodies;
     private static List<Color> colors;
-    private static List<Vector2> positions;
-    private static Vector2 force = Vector2.Zero;
+
 
     // Constructor for initialization
     static Scene()
     {
-        colors = new List<Color>() { Color.Red, Color.White, Color.White, Color.White };
+        colors = new List<Color>() { 
+            Color.White, 
+            Color.Red,
+            Color.Green,
+            Color.Blue,
+        };
 
         bodies = new List<PhysicsBody2D>();
-        positions = new List<Vector2>() { new Vector2(64, 320), new Vector2(480, 320), new Vector2(600, 320), new Vector2(480, 600) };
+       
     }
 
     // Ready function (Runs on first frame)
@@ -37,47 +40,6 @@ public class Scene : PhysicsWorld2D
         Draw();
         Collisions.HandleCollision(bodies);
 
-
-        // Move circles based on input
-        float dx = 0f;
-        float dy = 0f;
-        float magnitude = 30f;
-
-        if (Raylib.IsKeyDown(KeyboardKey.Left)) dx--;
-        else if (Raylib.IsKeyDown(KeyboardKey.Right)) dx++;
-
-        if (Raylib.IsKeyDown(KeyboardKey.Up)) dy--;
-        else if (Raylib.IsKeyDown(KeyboardKey.Down)) dy++;
-
-        float xAxis = Raylib.GetGamepadAxisMovement(0, GamepadAxis.LeftX);
-        float yAxis = Raylib.GetGamepadAxisMovement(0, GamepadAxis.LeftY);
-
-        if (Raylib.IsGamepadAvailable(0))
-        {
-            Vector2 direction = new Vector2(xAxis, yAxis) * (float)delta;
-            force = direction * magnitude;
-
-            RigidBody2D body = (RigidBody2D)bodies[0];
-            body.ApplyForce(force);
-        }
-
-        else
-        {
-            if (dx != 0 || dy != 0)
-            {
-                Vector2 direction = Vector2.Normalize(new Vector2(dx, dy)) * (float)delta;
-                force = direction * magnitude;
-
-          
-                if (bodies[0] is RigidBody2D)
-                {
-                    RigidBody2D body = (RigidBody2D)bodies[0];
-                    body.ApplyForce(force);
-                }
-
-            }
-        }
-
         foreach (PhysicsBody2D body in bodies)
         {
             if (body is RigidBody2D)
@@ -86,43 +48,30 @@ public class Scene : PhysicsWorld2D
                 rigidBody.RunComponents();
             }
         }
-
     }
 
     // Draw
     public static void Draw()
     {
         // Ensure bodies are created (call once or in Ready)
-        if (bodies.Count == 0)
-        {
-            for (int i = 0; i < positions.Count; i++)
-            {
-                RigidBody2D rigidBody;
-                StaticBody2D staticBody;
-                string errorMessage;
-
-                if (i < 3)
-                {
-                    // Create bodies using positions and colors
-                    CreateRigidBody(positions[i], 0f, Vector2.One, 1, 0.5f, ShapeType.Circle, 32f, 0f, 0f, out rigidBody, out errorMessage);
-
-                    // Add bodies to the list
-                    bodies.Add(rigidBody);
-                }
-
-                else
-                {
-                    CreateStaticBody(positions[i], 0f, Vector2.One, 0.5f, ShapeType.Box, 0f, 64f, 960f, out staticBody, out errorMessage);
-                    bodies.Add(staticBody);
-                }
-
-            }
+        if (bodies.Count == 0) { 
+            CreateStaticBody(new Vector2(480, 600), 0f, Vector2.One, 
+                0.5f, 64f, 960f, out StaticBody2D staticBody, out string errorMessage);
+            
+            bodies.Add(staticBody);  
         }
 
+        if (Raylib.IsMouseButtonPressed(MouseButton.Left)) {
+            CreateRigidBody(Raylib.GetMousePosition(), 0f, Vector2.One,
+                1f, 0.5f, 32f, out RigidBody2D rigidBody, out string errorMessage);
+
+            bodies.Add(rigidBody);
+        }
+
+
         // Update and draw each body
-        for (int i = 0; i < bodies.Count; i++)
-        {
-            RenderPhysicsObject(bodies[i], colors[i % 2]);
+        for (int i = 0; i < bodies.Count; i++) {
+            RenderPhysicsObject(bodies[i], colors[i % 4]);
         }
     }
 }
